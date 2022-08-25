6d8ea1bc3566e094689d98c1c3ab9c366646cb70
radha <radhavkrishna1994@gmail.com>
2022-08-25 11:04:16 +0530

    updated

---
+++ Day2_programs/src/com/training/base/Person.java
@@ -0,0 +1,39 @@
+package com.training.base;
+
+public class Person {
+
+	private String name;
+	private String address;
+	
+	public Person() {
+		super();       // calls base class default constructor  -- Object()
+	}
+
+	public Person(String name, String address) {
+		super();           // calls base class default constructor  -- Object()
+		this.name = name;
+		this.address = address;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getAddress() {
+		return address;
+	}
+
+	public void setAddress(String address) {
+		this.address = address;
+	}
+	
+	
+	public String toString()
+	{
+		return name+" "+address;
+	}
+}
---
+++ Day2_programs/src/com/training/clients/PersonClient.java
@@ -0,0 +1,14 @@
+package com.training.clients;
+
+import com.training.base.Person;
+
+public class PersonClient {
+
+	public static void main(String[] args) {
+		
+		Person p1=new Person("sam", "bangalore");
+		
+		System.out.println(p1);  //toString() 
+	}
+
+}
