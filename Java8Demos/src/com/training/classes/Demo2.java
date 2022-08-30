ac994dae457dd5b9c481592ed01476f4ce78a09b
radha <radhavkrishna1994@gmail.com>
2022-08-30 16:02:04 +0530

    updated

---
+++ Java8Demos/src/com/training/classes/Demo2.java
@@ -0,0 +1,16 @@
+package com.training.classes;
+
+import java.util.function.Function;
+
+public class Demo2 {
+
+	public static void main(String[] args) {
+	//square of a number
+		Function<Integer,Integer> square = (Integer input)->(input * input);
+		
+		System.out.println(square.apply(12));
+		
+
+	}
+
+}
