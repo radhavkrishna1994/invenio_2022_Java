089d80d75b31cac8586e3ba0525c752271fe5d39
radha <radhavkrishna1994@gmail.com>
2022-09-01 16:59:21 +0530

    updated

--- RestClassProject/src/main/java/com/training/BookResource.java
+++ RestClassProject/src/main/java/com/training/BookResource.java
@@ -4,24 +4,33 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
-import com.training.bookstore.Book2;
+
 import com.training.classes.Book;
+import com.training.db.BookDao;
 
 import jakarta.ws.rs.GET;
 import jakarta.ws.rs.POST;
 import jakarta.ws.rs.Path;
+import jakarta.ws.rs.PathParam;
 import jakarta.ws.rs.Produces;
 import jakarta.ws.rs.core.MediaType;
 
 @Path("/bookstore") // http://localhost:8080/RestClassProject/api/bookstore
 public class BookResource {
 	
-	private static List<Book> books = new ArrayList(Arrays.asList
-			(new Book(1234l, "C", 100.25, 100l),
-			new Book(4567l, "CPlus", 100.25, 100l),
-			new Book(1200l, "python", 220.25, 100l)
-				));
+	/*
+	 * private static List<Book> books = new ArrayList(Arrays.asList (new
+	 * Book(1234l, "C", 100.25, 100l), new Book(4567l, "CPlus", 100.25, 100l), new
+	 * Book(1200l, "python", 220.25, 100l) ));
+	 */
 
+	private BookDao bookDao;
+	
+	public BookResource()
+	{
+		bookDao = new BookDao();
+	}
+	
 	@GET
 	@Produces(MediaType.TEXT_PLAIN)
 	public String sayHello()
@@ -29,12 +38,20 @@ public class BookResource {
 		return "Welcome to my bookstore!!";
 	}
 	
-	@Path("/book")
+	//http://localhost:8080/RestClassProject/api/bookstore/book/isbn/1234
+	@Path("/book/isbn/{isbn}")
 	@Produces(MediaType.APPLICATION_JSON)
 	@GET
-	public Book getBook()
+	public Book getBook(@PathParam("isbn") Long isbn)
 	{
-		return new Book(1234l, "C", 150.25, 100l);
+		
+		for(Book book:bookDao.getBooks())
+		{
+			if(book.getIsbn().equals(isbn))
+				return book;
+		}
+			
+		return null;
 	}
 	
 	@Path("/books")
@@ -42,15 +59,17 @@ public class BookResource {
 	@GET
 	public List<Book> getBooks()
 	{
-		return books;
+		
+		return bookDao.getBooks();
 	}
 	
-	@Path("/book")
+	/*@Path("/book")
 	@POST
 	public Book addBook(Book book)
 	{
 		books.add(book);
 		return book;
 	}
+	*/
 	
 }
---
+++ RestClassProject/src/main/java/com/training/db/BookDao.java
@@ -0,0 +1,68 @@
+package com.training.db;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.Statement;
+import java.util.ArrayList;
+import java.util.List;
+
+import com.training.classes.Book;
+import com.training.db.DatabaseConnection;
+
+public class BookDao {
+	
+	private Connection connection;
+	
+	public BookDao()
+	{
+		connection=DatabaseConnection.getConnection();
+	}
+	public List<Book> getBooks()
+	{
+		List<Book> bookList = new ArrayList<>();
+		try {
+		//Statement
+		Statement stmt = connection.createStatement();
+		ResultSet rs = stmt.executeQuery("select * from book");
+		while(rs.next())
+		{
+			Book book=new Book();
+			book.setIsbn(rs.getLong(1));
+			book.setTitle(rs.getString(2));
+			book.setPrice(rs.getDouble(3));
+			book.setStock(rs.getLong(4));
+			bookList.add(book);
+			
+		}
+		return bookList;
+		}
+		catch(Exception e)
+		{
+			e.printStackTrace();
+			return null;
+		}
+		
+	}
+	
+	public int addBook(Book book)
+	{
+		PreparedStatement pstmt=null;
+		String template="insert into book values(?,?,?,?)";
+		try {
+			pstmt= connection.prepareStatement(template);
+			pstmt.setLong(1, book.getIsbn());
+			pstmt.setString(2, book.getTitle());
+			pstmt.setDouble(3, book.getPrice());
+			pstmt.setLong(4, book.getStock());
+			
+			return pstmt.executeUpdate();
+		}
+		catch(Exception e)
+		{
+			e.printStackTrace();
+			return 0;
+		}
+	}
+
+}
