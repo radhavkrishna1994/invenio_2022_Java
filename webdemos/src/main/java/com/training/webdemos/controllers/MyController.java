5f79e2d64b63bb6f115e0181fcae0f536d46e661
radha <radhavkrishna1994@gmail.com>
2022-09-02 17:18:19 +0530

    updated

--- webdemos/src/main/java/com/training/webdemos/controllers/MyController.java
+++ webdemos/src/main/java/com/training/webdemos/controllers/MyController.java
@@ -1,10 +1,14 @@
 package com.training.webdemos.controllers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class MyController {
 
+	@ResponseBody
+	@RequestMapping("/hello")  //http://localhost:8080/webdemos/hello
 	public String sayHello()
 	{
 		return "This is my controller";
