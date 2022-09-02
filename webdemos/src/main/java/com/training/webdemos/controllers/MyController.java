84919ed7b1bd2267c136943fce68d6d8102cab61
radha <radhavkrishna1994@gmail.com>
2022-09-02 17:48:42 +0530

    updated

--- webdemos/src/main/java/com/training/webdemos/controllers/MyController.java
+++ webdemos/src/main/java/com/training/webdemos/controllers/MyController.java
@@ -4,6 +4,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+
 @Controller
 public class MyController {
 
@@ -12,5 +13,17 @@ public class MyController {
 	public String sayHello()
 	{
 		return "This is my controller";
+	}
+	
+	@RequestMapping("/login")  //http://localhost:8080/webdemos/hello1
+	public String login()
+	{
+		return "login.jsp";
+	}
+	
+	@RequestMapping("/home")  //http://localhost:8080/webdemos/hello1
+	public String home()
+	{
+		return "home.jsp";
 	}
 }
---
+++ webdemos/src/main/webapp/login.jsp
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="ISO-8859-1">
+<title>Insert title here</title>
+</head>
+<body>
+<form action="home" method="get">
+
+<input type="text" name="name" placeholder="Enter username"/> <br>
+<input type="text" name="email" placeholder="Enter Email"/> <br>
+<input type="submit" value="submit"/>
+</form>
+</body>
+</html>
