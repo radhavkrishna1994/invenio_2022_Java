1f6b83100243ffa8fef846a63ad8b28e645862f0
radhavkrishna1994 <radhavkrishna1994@gmail.com>
2022-09-05 18:57:23 +0530

    updated

--- spring-boot-demo/src/main/java/com/training/SpringBootDemoApplication.java
+++ spring-boot-demo/src/main/java/com/training/SpringBootDemoApplication.java
@@ -26,21 +26,35 @@ public class SpringBootDemoApplication implements CommandLineRunner {
 	@Autowired
 	private BookRepo bookRepo;
 	
-	@PostConstruct
+	/*@PostConstruct
 	public void init()
 	{
 		bookRepo.save(new Book(1234l,"Let Us C",150.25,100l));
 		bookRepo.save(new Book(3456l,"Let Us CPlus",170.25,100l));
 		bookRepo.save(new Book(5432l,"Python",180.25,100l));
 		
-	}
+	}*/
 	
 	@Override
 	public void run(String... args) throws Exception {
 		
-		System.out.println("I am in command line runner");
+		//System.out.println("I am in command line runner");
 		
-		System.out.println(config.getMessage());
+		//System.out.println(config.getMessage());
+		
+		System.out.println(bookRepo.findByIsbn(1234l));
+		System.out.println(bookRepo.findByTitleLike("%c%"));
+		bookRepo.findByPriceGreaterThan(100.0)
+		.forEach(b->System.out.println(b));
+		
+		System.out.println("-------------------------");
+		
+		bookRepo.getBooksGtPrice(200.0)
+		.forEach(b->System.out.println(b));
+		
+		System.out.println("-------------------------");
+		
+		System.out.println(bookRepo.updateStock(50l, 120l));
 	}
 	
 	
