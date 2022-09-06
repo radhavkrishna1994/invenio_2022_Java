1e0775966743fcc157ba6370e0e17a360083688b
radhavkrishna1994 <radhavkrishna1994@gmail.com>
2022-09-06 17:42:31 +0530

    updated

--- spring-boot-security-demo-db/src/main/java/com/training/model/MyUser.java
+++ spring-boot-security-demo-db/src/main/java/com/training/model/MyUser.java
@@ -1,7 +1,9 @@
 package com.training.model;
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.Id;
+
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
@@ -14,6 +16,7 @@ import lombok.NoArgsConstructor;
 public class MyUser {
 
 	@Id
+	@Column(name = "username",length = 100)
 	private String username;
 	private String password;
 	private String role;
