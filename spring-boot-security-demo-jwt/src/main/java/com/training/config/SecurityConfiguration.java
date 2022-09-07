dcd5eb57f7478eb61e64b207dc759575515211a6
radhavkrishna1994 <radhavkrishna1994@gmail.com>
2022-09-07 14:10:23 +0530

    updated

--- spring-boot-security-demo-jwt/src/main/java/com/training/config/SecurityConfiguration.java
+++ spring-boot-security-demo-jwt/src/main/java/com/training/config/SecurityConfiguration.java
@@ -3,12 +3,16 @@ package com.training.config;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
+import com.training.filters.JwtFilter;
 import com.training.services.MyUserDetailsService;
 
 @Configuration
@@ -50,19 +54,33 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter{
 		return encoder;
 	}
 
-	//authorization
+	@Autowired
+	private JwtFilter jwtFilter;
+	
 	public void configure(HttpSecurity http) throws Exception
 	{
-		log.info("I am in Authorization");
+		log.info("In Authorization....");
+		
 		http
+		.csrf().disable()
 		.authorizeRequests()
+		.antMatchers("/authenticate")
+		.permitAll()
+		.antMatchers("/user/**")
+		//.hasRole("USER")
+		.hasAnyRole("USER","ADMIN")
 		.antMatchers("/admin/**")
 		.hasRole("ADMIN")
-		.antMatchers("/user/**")
-		.hasRole("USER")
-		.and()
-		.formLogin();
+		.anyRequest().authenticated();
 		
+		http.addFilterBefore(jwtFilter, UsernamePasswordAuthenticationFilter.class);
+	}
+	
+	
+	@Bean
+	public AuthenticationManager getAuthManager() throws Exception
+	{
+		return super.authenticationManager();
 	}
 	
 	
